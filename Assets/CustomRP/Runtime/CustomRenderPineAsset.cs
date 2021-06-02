3f8ca534e3ff5b1783d79cbbc93fa57901a93168
DESKTOP-5S2UP63\admin <xtu@zju.edu.cn>
2021-06-02 19:09:31 +0800

    加入烘焙

--- Assets/CustomRP/Runtime/CustomRenderPineAsset.cs
+++ Assets/CustomRP/Runtime/CustomRenderPineAsset.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-
-//自定义渲染管线资产
+/// <summary>
+/// 自定义渲染管线资产
+/// </summary>
+//该标签会在你在Project下右键->Asset/Create菜单中添加一个新的子菜单
 [CreateAssetMenu(menuName = "Rendering/CreateCustomRenderPipeline")]
-public class CustomRenderPineAsset : RenderPipelineAsset
+public class CustomRenderPipeAsset : RenderPipelineAsset
 {
     //设置批处理启用状态
     [SerializeField]
     bool useDynamicBatching = true, useGPUInstancing = true, useSRPBatcher = true;
-
-    //阴影设置
+    //是否使用逐对象光照
+    [SerializeField]
+    bool useLightsPerObject = true;
+    //阴影配置
     [SerializeField]
     ShadowSettings shadows = default;
 
     //重写抽象方法，需要返回一个RenderPipeline实例对象
     protected override RenderPipeline CreatePipeline()
     {
-        return new CustomRenderPipeline(useDynamicBatching, useGPUInstancing, useSRPBatcher, shadows);
+        return new CustomRenderPipeline(useDynamicBatching, useGPUInstancing, useSRPBatcher, useLightsPerObject, shadows);
     }
 }
